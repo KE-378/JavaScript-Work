d525b6859862122a75c63162367a4a771c3422fd
KE-378 <syedharisqazi@gmail.com>
2024-02-22 18:25:39 +0500

    chapter 01 completed

---
+++ Chapter_01/index.js
@@ -0,0 +1,12 @@
+//Q#01
+alert("AssalamuAlaikum God Bless U")
+
+//Q#02
+alert("Error! Please enter a Valid password")
+
+//Q#03
+alert("Welcome to JSland .\n Happy Coding! ")
+
+//Q#04
+alert("Welcome to JSland")
+alert("Happy Coding!")
