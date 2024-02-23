7eeeec282446d0c55e22c900e78e463cf32e22a6
KE-378 <syedharisqazi@gmail.com>
2024-02-24 01:29:14 +0500

    Chapter 04 donw

---
+++ Chapter_03/Chapter_04/index.js
@@ -0,0 +1,25 @@
+//Q#01
+
+var variable1,variable2,variable3;
+
+//Q#02
+
+list of 05 legal variable names
+1- FirstName
+02-_userCount
+03-$totalAmount
+04- myVar
+05-a123
+
+List of 05 illegal variable
+
+01-my-variable  start with hiphen
+02-123abc   start with any digit
+03- @username a username conatin  any special characrter
+04 let
+05 for
+
+document.write("Variable name can only contain numbers $ for example $total amount & a123")
+document.write("Variable must begin with a letter i.e a,$ ")
+document.write("variable names are case sensitive")
+document.write("Variable names should not be JS keywords")
