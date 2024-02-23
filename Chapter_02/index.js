aeedfa8be0c86a801a6c3beb81cf996747ce3a79
KE-378 <syedharisqazi@gmail.com>
2024-02-24 00:06:12 +0500

    Chapter 2 done

---
+++ Chapter_02/index.js
@@ -0,0 +1,39 @@
+//Q#01
+
+var username
+
+//Q#02
+
+var myName="Syed Haris uddin Qazi";
+
+//Q#03
+var Greeting="Hello World";
+alert(Greeting)
+
+//Q#04
+var name="Syed Haris";
+var age=34;
+var msg="Certified mobile application product"
+
+alert(name)
+alert(age)
+alert(msg)
+
+//Q#05
+var Pizza="PIZZA \n PIZZ \n Piz \n Pi" 
+alert(Pizza)
+
+//Q#06
+
+var email="example@example.com"
+var msg="My email address is"
+alert(msg+ email)
+
+// Q#07
+var book="I am trying to learn book from A smater way to learn Java Script"
+alert(book)
+
+// Q#09
+
+var symbol="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(symbol)
