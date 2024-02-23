b885a16611234db58dbd48ba744b74aee8942ba0
KE-378 <syedharisqazi@gmail.com>
2024-02-24 00:55:29 +0500

    chapter 03 done

---
+++ Chapter_03/index.js
@@ -0,0 +1,19 @@
+//Q#01
+
+var age=34
+alert("My age is " + age )
+
+//Q#02
+var times=14
+alert("You have visited"+times)
+
+//Q#03
+
+var birthyear=1989;
+document.write("My birth year is" + birthyear)
+
+//Q#04
+
+var website = "www.shopping.com"
+
+document.write("Mr. Haris purchsed 5shirts & 2 paints from" + website)
