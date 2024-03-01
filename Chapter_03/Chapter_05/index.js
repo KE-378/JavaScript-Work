8220354307aac6df20da4dfbe4d5d6a454484e42
KE-378 <syedharisqazi@gmail.com>
2024-03-01 19:55:57 +0500

    adding chapter 05

---
+++ Chapter_03/Chapter_05/index.js
@@ -0,0 +1,21 @@
+
+
+var num1=80
+var num2=20
+var addition=num1+num2
+var subst=num1-num2
+var multiply=num1*num2
+var divide=num1/num2
+var modulus=num1%num2
+var exponent=8**4
+
+console.log(addition)
+console.log(subst)
+console.log(multiply)
+console.log(divide)
+console.log(modulus)
+console.log(exponent)
+console.log(multiply*addition-subst)
+console.log(divide*addition-subst)
+console.log(exponent*addition-subst)
+console.log(modulus*addition-subst)
