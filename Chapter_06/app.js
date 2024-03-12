26f4aa35585de4c24b48609c0290abe79029ce16
KE-378 <syedharisqazi@gmail.com>
2024-03-13 00:05:56 +0500

    adding chapter 6

---
+++ Chapter_06/app.js
@@ -0,0 +1,37 @@
+
+Concatenate//
+
+var firstName= "Syed Haris Uddin"
+var lastName= "Qazi"
+console.log(firstName+" "+lastName)
+ 
+var firstName= "Syed Hammad Uddin"
+var lastName= "Qazi"
+console.log(firstName+" "+lastName)
+
+Prompt//
+
+var x = prompt("Where Mr Haris lives");
+var Y = ("Malir");
+
+if (x===Y){
+    console.log("Correct");
+}
+
+var Maths=+prompt ("Maths Obtained number")
+var Urdu= +prompt("Urdu Obtained number")
+var Science=+prompt("Science Obtained number")
+console.log(Maths+Urdu+Science)
+
+var percentage=(Maths+Urdu+Science)/300*100
+console.log(percentage)
+
+console.log("Your Percentage is"+ percentage+" "+ "%")
+
+var milk=+prompt("What is the Prize of Milk")
+var Honey=+prompt("What is the price of Honey")
+console.log(milk+Honey)
+var percentage=(milk+Honey)/500*100
+console.log(percentage)
+console.log("What is your total amount"+percentage+"%")
+
