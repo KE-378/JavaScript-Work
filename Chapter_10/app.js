1a3e56829274f1c1525d6c5f7de529a2a70ae176
KE-378 <syedharisqazi@gmail.com>
2024-03-20 00:45:44 +0500

    add

---
+++ Chapter_10/app.js
@@ -0,0 +1,36 @@
+
+// for (var i = 1; i<=7; i++){
+//     console.log( i+ 'Chakkar')
+// }
+
+
+// for( var i = 1; i<=20; i++){
+//     console.log('2 *' + 'i'+'='+ 2*i)
+// }
+
+// for (var i = 1; i<=20; i++){
+//     console.log('3 *' + 'i' + '=' + 3*1)
+// }
+
+// var i = prompt("type table number")
+// for ( var i = 1; i<=7; i++){
+//     console.log('i *' + 1 + '=' + i*7)
+// }
+
+
+// var tableNumber = prompt('enter table number')
+// for(var i = 1; i<50; i++){
+//     document.write(tableNumber + '*' + 'i' + '=' + tableNumber*i + '<br>')
+// }
+
+// var tableNumber = prompt('Enter Table Number')
+
+// for( var i=1; i<=10; i++){
+//     document.write(tableNumber + '*' + 'i' + '=' + tableNumber * i + '<br>')
+// }
+
+
+var weekDays = ["mon" , "tue" , "wed" , "thur" , "fri" , "sat"]
+for (var i=0; i<= weekDays.length; i++){
+    document.write(weekDays[i]+ "<br>")
+}
